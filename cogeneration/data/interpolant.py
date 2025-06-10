inpainting fix: zero scaffolds after re-center, during sampling motif guidance masking and cloning model/sample states

cogeneration/data/interpolant.py
@@ -1211,7 +1211,7 @@ class Interpolant:
 
         return aatypes_t
 
-    def sample_step(
+    def sample_single_step(
         self,
         noisy_batch: NoisyFeatures,
         true_feats: BatchTrueFeatures,
@@ -1279,15 +1279,19 @@ class Interpolant:
                 pred_aatypes_1, true_feats.aatypes, scaffold_mask
             )
 
-            # For inpainting with guidance, set motif structure to t=1
+            # TODO - consider whether we should first align (i.e. rotate)
+            #   the pred structure to known motifs, rather than simply substituting them in.
+            #   I don't think FrameFlow guidance did this, but the twisting variant sort of does?
+
+            # For inpainting with guidance, fix known motifs of predicted structure
             # so that we interpolate toward the known motifs. Also for self-conditioning.
-            pred_trans_1 = mask_blend_2d(pred_trans_1, true_feats.trans, diffuse_mask)
+            pred_trans_1 = mask_blend_2d(pred_trans_1, true_feats.trans, scaffold_mask)
             pred_rotmats_1 = mask_blend_3d(
-                pred_rotmats_1, true_feats.rotmats, diffuse_mask
+                pred_rotmats_1, true_feats.rotmats, scaffold_mask
             )
             if pred_torsions_1 is not None:
                 pred_torsions_1 = mask_blend_3d(
-                    pred_torsions_1, true_feats.torsions, diffuse_mask
+                    pred_torsions_1, true_feats.torsions, scaffold_mask
                 )
         elif task == InferenceTask.forward_folding:
             # scale logits during integration, assumes will `softmax`
@@ -1605,8 +1609,9 @@ class Interpolant:
         ts = torch.linspace(self.cfg.min_t, 1.0, self.cfg.sampling.num_timesteps)
         step_idx = 0
 
-        # We will integrate in a loop over ts (handling the last step after the loop)
-        # t_1 is the current time, t_2 is the next time
+        # We will integrate in a loop over ts, handling the last step after the loop.
+        # t_1 is the current time (handle updating ourselves at end of loop).
+        # t_2 is the next time.
         t_1 = ts[0]
         for t_2 in ts[1:]:
             # Determine time for each domain
@@ -1644,7 +1649,7 @@ class Interpolant:
                     raise ValueError(f"Unknown task {task}")
 
             # Take a single step, updating the batch in place
-            batch, model_step, protein_step = self.sample_step(
+            batch, model_step, sample_step = self.sample_single_step(
                 noisy_batch=batch,
                 true_feats=true_feats,
                 model=model,
@@ -1655,7 +1660,7 @@ class Interpolant:
             )
 
             model_trajectory.append(model_step)
-            sample_trajectory.append(protein_step)
+            sample_trajectory.append(sample_step)
 
             # Update t_1 to t_2 for the next step
             t_1 = t_2
@@ -1670,7 +1675,7 @@ class Interpolant:
         batch[nbp.r3_t] = t
         batch[nbp.cat_t] = t
 
-        batch, model_step, protein_step = self.sample_step(
+        batch, model_step, sample_step = self.sample_single_step(
             noisy_batch=batch,
             true_feats=true_feats,
             model=model,
@@ -1681,14 +1686,14 @@ class Interpolant:
         )
 
         model_trajectory.append(model_step)
-        sample_trajectory.append(protein_step)
+        sample_trajectory.append(sample_step)
 
         # If FK steering is enabled, pick the best particle per sample
         # TODO - allow passing through all the particles. Ensure each is handled properly.
         _, best_idx = self.resampler.best_particle_in_batch(batch=batch)
         if best_idx is not None:
             # Select the best particle for each sample
-            sample_trajectory = sample_trajectory.select_batch_idx(best_idx)
             model_trajectory = model_trajectory.select_batch_idx(best_idx)
+            sample_trajectory = sample_trajectory.select_batch_idx(best_idx)
 
         return sample_trajectory, model_trajectory

cogeneration/dataset/datasets.py
@@ -19,6 +19,7 @@ from cogeneration.config.base import (
 )
 from cogeneration.data import data_transforms, rigid_utils
 from cogeneration.data.const import seq_to_aatype
+from cogeneration.data.noise_mask import mask_blend_2d
 from cogeneration.dataset.filterer import DatasetFilterer
 from cogeneration.dataset.motif_factory import (
     ChainBreak,
@@ -197,10 +198,10 @@ class BatchFeaturizer:
                     new_feats[prop][ns:ne] = feats[prop][os:oe]
 
                 # mark motif
-                new_feats[bp.motif_mask][ns:ne] = 1.0
+                new_feats[bp.motif_mask][ns:ne] = 1
                 # mark diffusable for inpainting with guidance
                 # TODO(inpainting-fixed) mark fixed
-                new_feats[bp.diffuse_mask][ns:ne] = 1.0
+                new_feats[bp.diffuse_mask][ns:ne] = 1
                 # enforce idx
                 new_feats[bp.chain_idx][ns:ne] = current_chain_idx
                 new_feats[bp.res_idx][ns:ne] = torch.arange(
@@ -211,9 +212,9 @@ class BatchFeaturizer:
                 # Scaffolds mostly retain default `empty_feats` values.
 
                 # mark scaffold
-                new_feats[bp.motif_mask][ns:ne] = 0.0
+                new_feats[bp.motif_mask][ns:ne] = 0
                 # ensure marked to be diffused
-                new_feats[bp.diffuse_mask][ns:ne] = 1.0
+                new_feats[bp.diffuse_mask][ns:ne] = 1
                 # enforce idx
                 new_feats[bp.chain_idx][ns:ne] = current_chain_idx
                 new_feats[bp.res_idx][ns:ne] = torch.arange(
@@ -397,8 +398,14 @@ class BatchFeaturizer:
 
         # Centering
         if bp.motif_mask in feats and (feats[bp.motif_mask] == 1).any():
-            # Center the motifs using motif_mask
+            # Center the motifs using motif_mask, and re-zero the scaffolds,
+            # to prevent biasing how scaffolds are placed relative to motifs.
             BatchFeaturizer.recenter_structure(feats, mask=feats[bp.motif_mask])
+            feats[bp.trans_1] = mask_blend_2d(
+                feats[bp.trans_1],
+                torch.zeros_like(feats[bp.trans_1]),
+                mask=feats[bp.motif_mask],
+            )
         else:
             # Center the whole structure
             BatchFeaturizer.recenter_structure(feats)

cogeneration/type/batch.py
@@ -110,14 +110,14 @@ def empty_feats(N: int, task: DataTask = DataTask.hallucination) -> BatchFeature
         BatchProp.chain_idx: torch.ones(N),
         BatchProp.res_idx: torch.arange(1, N + 1),
         BatchProp.res_plddt: torch.full((N,), 100.0),
-        BatchProp.diffuse_mask: torch.ones(N),
-        BatchProp.plddt_mask: torch.ones(N),
+        BatchProp.diffuse_mask: torch.ones(N).int(),
+        BatchProp.plddt_mask: torch.ones(N).int(),
         BatchProp.pdb_name: "",
         BatchProp.csv_idx: torch.tensor([1], dtype=torch.long),
         BatchProp.sample_id: 0,  # inference only but no impact to training / model
     }
 
     if task == DataTask.inpainting:
-        feats[BatchProp.motif_mask] = torch.zeros(N)
+        feats[BatchProp.motif_mask] = torch.zeros(N).int()
 
     return feats

test/data/interpolant_sample_test.py
@@ -3,7 +3,7 @@ import torch
 
 from cogeneration.config.base import Config
 from cogeneration.data.interpolant import Interpolant
-from cogeneration.data.noise_mask import torsions_empty
+from cogeneration.dataset.test_utils import mock_feats
 from cogeneration.type.batch import BatchProp as bp
 from cogeneration.type.batch import NoisyBatchProp as nbp
 from cogeneration.type.batch import NoisyFeatures
@@ -14,7 +14,7 @@ from cogeneration.type.task import InferenceTask
 class TestInterpolantSample:
     """Test suite for Interpolant.sample()."""
 
-    def _run_sample(self, cfg: Config, batch, task: InferenceTask):
+    def _run_sample(self, cfg: Config, batch: NoisyFeatures, task: InferenceTask):
         # ensure don't use training interpolant, will mess up shapes
         cfg.interpolant.sampling.num_timesteps = 7
 
@@ -26,13 +26,24 @@ class TestInterpolantSample:
         T = cfg.inference.interpolant.sampling.num_timesteps
 
         # Dummy model
-        # TODO consider returning noise rather than the same `_t` values each step
         class ModelStub:
             def __call__(self, noisy_batch: NoisyFeatures):
                 trans = noisy_batch[nbp.trans_t]
                 rotmats = noisy_batch[nbp.rotmats_t]
                 torsions = noisy_batch[nbp.torsions_t]
                 aatypes = noisy_batch[nbp.aatypes_t].long()
+
+                # low fidelity corruptions
+                trans = trans + torch.randn_like(noisy_batch[nbp.trans_t])
+                rotmats = rotmats + torch.randn_like(noisy_batch[nbp.rotmats_t]) * 0.25
+                torsions = (
+                    torsions + torch.randn_like(noisy_batch[nbp.torsions_t]) * 0.25
+                )
+                aatypes = aatypes + torch.randint(
+                    0, num_tokens, aatypes.shape, dtype=torch.long
+                )
+                aatypes = aatypes % num_tokens
+
                 logits = torch.nn.functional.one_hot(
                     aatypes, num_classes=num_tokens
                 ).float()
@@ -128,27 +139,49 @@ class TestInterpolantSample:
         batch = next(iter(dataloader))
         self._run_sample(cfg=cfg, batch=batch, task=task)
 
-    def test_inpainting_preserves_motif_sequence_in_sampling(
+    def test_inpainting_preserves_motif_in_sampling(
         self, mock_cfg_uninterpolated, mock_pred_inpainting_dataloader
     ):
         """
-        Inpainting sampling should preserve amino acid sequence at motif positions
+        Inpainting sampling should preserve motif positions
         """
         mock_cfg_uninterpolated.inference.task = InferenceTask.inpainting
         mock_cfg_uninterpolated.interpolant.sampling.num_timesteps = 3
         cfg = mock_cfg_uninterpolated.interpolate()
 
         batch = next(iter(mock_pred_inpainting_dataloader))
-        _, model_traj = self._run_sample(
+        sample_traj, model_traj = self._run_sample(
             cfg=cfg, batch=batch, task=InferenceTask.inpainting
         )
 
-        final_aa = model_traj.amino_acids[:, -1]
-        orig_aa = batch[bp.aatypes_1]
         motif_sel = batch[bp.motif_mask].bool()
+
+        # sanity check, don't expect model and sample trajectories to be equal in motifs.
+        # if they are, likely accidentally overwriting the model states.
+        assert not torch.equal(
+            model_traj[-1].aatypes[motif_sel],
+            sample_traj[-1].aatypes[motif_sel],
+        ), "Model and sample trajectories should not be equal in motifs"
+        assert not torch.allclose(
+            model_traj[-1].trans[motif_sel],
+            sample_traj[-1].trans[motif_sel],
+        ), "Model and sample trajectories should not be equal in motifs"
+
+        # compare aatypes - should be fixed
+        final_aa = sample_traj[-1].aatypes
+        orig_aa = batch[bp.aatypes_1]
         assert torch.equal(
             final_aa[motif_sel], orig_aa[motif_sel]
         ), "Motif amino acids should be preserved in inpainting sampling"
+
+        # compare structure - should match due to "guidance"
+        # But account for COM changing.
+        final_trans = sample_traj[-1].trans
+        orig_trans = batch[bp.trans_1]
+        com_shift = (final_trans[motif_sel] - orig_trans[motif_sel])[0]
+        assert torch.allclose(
+            final_trans[motif_sel] - orig_trans[motif_sel], com_shift.unsqueeze(0)
+        ), "Motif translations should be preserved in inpainting sampling"
 
     @pytest.mark.parametrize(
         "task",